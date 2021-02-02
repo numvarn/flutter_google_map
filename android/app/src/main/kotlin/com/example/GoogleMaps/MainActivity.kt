21a939aae0c7cf4c62d3420c6017815fe3d264e1
numvarn <phisan.shukkhi@gmail.com>
2021-02-02 20:48:41 +0700

    release app

---
+++ android/app/src/main/kotlin/com/example/GoogleMaps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.GoogleMaps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/map.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapsPage extends StatefulWidget {
+  @override
+  _MapsPageState createState() => _MapsPageState();
+}
+
+class _MapsPageState extends State<MapsPage> {
+  Position userLocation;
+  GoogleMapController mapController;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  Future<Position> _getLocation() async {
+    try {
+      userLocation = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+    } catch (e) {
+      userLocation = null;
+    }
+    return userLocation;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Google Maps'),
+      ),
+      body: FutureBuilder(
+        future: _getLocation(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return GoogleMap(
+              mapType: MapType.normal,
+              onMapCreated: _onMapCreated,
+              myLocationEnabled: true,
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(userLocation.latitude, userLocation.longitude),
+                  zoom: 15),
+            );
+          } else {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          mapController.animateCamera(CameraUpdate.newLatLngZoom(
+              LatLng(userLocation.latitude, userLocation.longitude), 18));
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text(
+                    'Your location has been send !\nlat: ${userLocation.latitude} long: ${userLocation.longitude} '),
+              );
+            },
+          );
+        },
+        label: Text("Send Location"),
+        icon: Icon(Icons.near_me),
+      ),
+    );
+  }
+}
